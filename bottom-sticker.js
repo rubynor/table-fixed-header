0bcb7f03397b9f7f6a342e20f278386a7726853a
Jeffen Cheung <zhanfeng_zhang@163.com>
2016-04-11 15:03:11 +0800

    optimization: add process for config options.

--- bottom-sticker.js
+++ bottom-sticker.js
@@ -10,48 +10,54 @@
 
 $.fn.bottomSticker = function (options) {
 	var config = {
+		bottom: '0px',
 		width: '100%'
 	};
 	if (options){ $.extend(config, options); }
 
 	return this.each( function() {
-	var o = $(this)
-		, cId = o.attr("id") + "-copy";
-	var c = "." + cId;
+		var o = $(this)
+			, cId = o.attr("id") + "-copy";
+		var c = "." + cId;
 
-	var $win = $(window)
-		, isFixed = 0;
+		var $win = $(window)
+			, isFixed = 0;
 
-	function processScroll() {
-		var scrollTop = $win.scrollTop()
-				, cTop = $(c).offset().top
-				, oTop = o.offset().top;
-		if (oTop > cTop && !isFixed) {
-			isFixed = 1;
-		} else if (oTop <= cTop && isFixed) {
-			isFixed = 0;
+		function processScroll() {
+			var scrollTop = $win.scrollTop()
+					, cTop = $(c).offset().top
+					, oTop = o.offset().top;
+			if (oTop > cTop && !isFixed) {
+				isFixed = 1;
+			} else if (oTop <= cTop && isFixed) {
+				isFixed = 0;
+			}
+
+			isFixed ? $(c).show().width(getWidth())
+			        : $(c).hide();
+
 		}
-		isFixed ? $(c).show()
-		        : $(c).hide();
+		function getWidth() {
+			var width = o.width();
+			if (width == 0)
+				width = config['width'];
+			return width;
+		}
 
-	}
+		$win.on('scroll', processScroll);
+		$win.on('resize', processScroll);
 
-	$win.on('scroll', processScroll);
-	$win.on('resize', processScroll);
+		var clone =  o.clone(true)
+			.attr("id", cId)
+			.addClass(cId)
+			.css({'position': 'fixed'
+				, 'bottom': config['bottom']
+				, 'width': getWidth()});
+			o.after(clone);
 
-	var width = config['width'];
-	width = o.width();
-	var clone =  o.clone(true)
-		.attr("id", cId)
-		.addClass(cId)
-		.css({'position': 'fixed'
-			, 'bottom': '0px'
-			, 'width': width});
-		o.after(clone);
+		processScroll();
 
-	processScroll();
-
- });
+	}); // return each
 };
 
 })(jQuery);
